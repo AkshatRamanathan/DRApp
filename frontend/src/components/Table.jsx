ad44807b894790738b14cbd91a661a0de8b7b5fe
AkshatRamanathan <ramanathan.ak@northeastern.edu>
2024-03-08 13:35:27 -0500

    FE: generic table creation fixes for data rendering

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -6,11 +6,11 @@ import { useParams } from 'react-router-dom';
 
 export default function Table() {
     const { entityType } = useParams();
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({});
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(`/api/${entityType}/all`);
-            const newData = await response.json();
+            const newData = await response.json(); //newData has data and columns
             if (!newData) {
                 setData({});
 
@@ -35,23 +35,24 @@ export default function Table() {
     }
     return (
         <Card style={{ height: "550px" }}>
-            <Card.Header>My {entityType?.toString()}</Card.Header>
+            <Card.Header>My {entityType?.toString().toUpperCase()}</Card.Header>
             <Card.Body>
                 <BTable striped bordered hover>
                     <thead>
                         <tr>
-                            <th>Title</th>
-                            <th>Like Counts</th>
-                            <th>Actions</th>
+                            {data?.columns?.map((col) => { return <th>{col.toUpperCase()}</th> })}
+                            <th>ACTIONS</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            data?.map((dataItem, idx) => {
-                                return <tr onClick={() => { location.href = `/dashboard/${entityType}/${dataItem._id}` }} key={idx}>
-                                    <td>{dataItem.title}</td>
-                                    <td>{dataItem.likeCount}</td>
-                                    <td><i onClick={(e) => { handleDelete(e, dataItem) }} className="btn btn-primary bi bi-trash3"></i>&nbsp;<i onClick={(e) => { handleEdit(e, dataItem) }} className="btn  btn-primary bi bi-pencil-square"></i></td>
+                            data?.data?.map((dataItem, idx) => {
+                                return <tr onClick={() => { entityType === 'posts' ? location.href = `/dashboard/${entityType}/${dataItem._id}` : null }} key={idx}>
+                                    {
+                                        data.columns.map(col => <td>{dataItem[col]}</td>)
+                                    }
+                                    <td><i onClick={(e) => { handleDelete(e, dataItem) }} className="btn btn-primary bi bi-trash3"></i>&nbsp;
+                                        {entityType === 'posts' ? <i onClick={(e) => { handleEdit(e, dataItem) }} className="btn  btn-primary bi bi-pencil-square"></i> : null}</td>
                                 </tr>
                             })
                         }
---
+++ frontend/src/components/Util.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';	
+import Card from 'react-bootstrap/Card';	
+
+export default function Util({ util }) {	
+
+  return (	
+    <Card style={{ height: '15rem' }}>	
+      <Card.Header as="h5">{util}</Card.Header>	
+      <Card.Body>	
+        sdfsdfsdf	
+      </Card.Body>	
+    </Card>	
+  )	
+}	
